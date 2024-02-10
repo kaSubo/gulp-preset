76942b7dc8aa67daf13e576d7e407dcbf6818468
Anton Nikityuk <159650614+kaSubo@users.noreply.github.com>
2024-02-11 00:18:51 +0500

    Add files via upload

---
+++ gulp/dev.js
@@ -0,0 +1,129 @@
+const gulp 				= require('gulp');
+const fileInclude = require('gulp-file-include');
+const panini 		 	 = require('panini');
+const sass 			 	= require('gulp-sass')(require('sass'));
+const concat 			= require('gulp-concat');
+const sassGlob 	 	= require('gulp-sass-glob');
+const server 		 	= require('gulp-server-livereload');
+const clean 	   	= require('gulp-clean');
+const fs 				 	= require('fs');
+const sourceMaps 	= require('gulp-sourcemaps');
+const plumber 	 	= require('gulp-plumber');
+const notify 		 	= require('gulp-notify');
+const webpack 	 	= require('webpack-stream');
+const babel 		 	= require('gulp-babel');
+const imagemin 	 	= require('gulp-imagemin');
+const changed 	 	= require('gulp-changed');
+
+gulp.task('clean:dev', function (done) {
+	if (fs.existsSync('./build/')) {
+		return gulp
+			.src('./build/', { read: false })
+			.pipe(clean({ force: true }));
+	}
+	done();
+});
+
+const fileIncludeSetting = {
+	prefix: '@@',
+	basepath: '@file',
+};
+
+const plumberNotify = (title) => {
+	return {
+		errorHandler: notify.onError({
+			title: title,
+			message: 'Error <%= error.message %>',
+			sound: false,
+		}),
+	};
+};
+
+gulp.task('html:dev', function () {
+	panini.refresh();
+	return (
+		gulp
+		.src([
+			'./app/html/**/*.html',
+			'!./app/html/blocks/*.html',
+			'!./app/html/partials/*.html',
+			'!./app/html/layouts/*.html'
+	])
+			.pipe(panini({
+				root: 		 './app/',
+				layouts: 	 './app/html/layouts/',
+				partials:  './app/html/partials/',
+				helpers: 	 './app/html/helpers/',
+				data: 		 './app/html/data/',
+			}))
+			.pipe(changed('./build/', { hasChanged: changed.compareContents }))
+			.pipe(plumber(plumberNotify('HTML')))
+			.pipe(fileInclude(fileIncludeSetting))
+			.pipe(gulp.dest('./build/'))
+	);
+});
+
+gulp.task('sass:dev', function () {
+	return (
+		gulp
+			.src('./app/scss/*.scss')
+			.pipe(changed('./build/css/'))
+			.pipe(plumber(plumberNotify('SCSS')))
+			.pipe(sourceMaps.init())
+			.pipe(sassGlob())
+			.pipe(concat('style.min.css'))
+			.pipe(sass({ outputStyle: 'compressed' }))
+			.pipe(sourceMaps.write())
+			.pipe(gulp.dest('./build/css/'))
+	);
+});
+
+gulp.task('images:dev', function () {
+	return gulp
+		.src('./app/images/**/*')
+		.pipe(changed('./build/images/'))
+		// .pipe(imagemin({ verbose: true }))
+		.pipe(gulp.dest('./build/images/'));
+});
+
+gulp.task('fonts:dev', function () {
+	return gulp
+		.src('./app/fonts/**/*')
+		.pipe(changed('./build/fonts/'))
+		.pipe(gulp.dest('./build/fonts/'));
+});
+
+gulp.task('files:dev', function () {
+	return gulp
+		.src('./app/files/**/*')
+		.pipe(changed('./build/files/'))
+		.pipe(gulp.dest('./build/files/'));
+});
+
+gulp.task('js:dev', function () {
+	return gulp
+		.src('./app/js/*.js')
+		.pipe(changed('./build/js/'))
+		.pipe(plumber(plumberNotify('JS')))
+		// .pipe(babel())
+		.pipe(webpack(require('./../webpack.config.js')))
+		.pipe(gulp.dest('./build/js/'));
+});
+
+const serverOptions = {
+	livereload: true,
+	open: true,
+};
+
+gulp.task('server:dev', function () {
+	return gulp.src('./build/').pipe(server(serverOptions));
+});
+
+gulp.task('watch:dev', function () {
+	gulp.watch('./app/scss/**/*.scss', gulp.parallel('sass:dev'));
+	gulp.watch('./app/html/**/*.html', gulp.parallel('html:dev'));
+	gulp.watch('./app/images**/*', gulp.parallel('images:dev'));
+	gulp.watch('./app/fonts/**/*', gulp.parallel('fonts:dev'));
+	gulp.watch('./app/files/**/*', gulp.parallel('files:dev'));
+	gulp.watch('./app/js/**/*.js', gulp.parallel('js:dev'));
+});
